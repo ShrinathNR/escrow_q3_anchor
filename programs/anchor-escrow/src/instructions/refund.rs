bedb8fb0e830885e6d73f7b4b4dc4defdf7509bf
ShrinathNR <nr.shrinath@gmail.com>
2025-07-29 17:37:12 +0530

    add test

--- programs/anchor-escrow/src/instructions/refund.rs
+++ programs/anchor-escrow/src/instructions/refund.rs
@@ -7,25 +7,21 @@ use anchor_spl::{
         TransferChecked,
     },
 };
+
 use crate::Escrow;
 
 #[derive(Accounts)]
 pub struct Refund<'info> {
     #[account(mut)]
-    pub maker: Signer<'info>,
-
-    #[account(
-        mint::token_program = token_program
-    )]
-    pub mint_a: InterfaceAccount<'info, Mint>,
+    maker: Signer<'info>,
+    mint_a: InterfaceAccount<'info, Mint>,
     #[account(
         mut,
         associated_token::mint = mint_a,
         associated_token::authority = maker,
         associated_token::token_program = token_program
-
     )]
-    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
+    maker_ata_a: InterfaceAccount<'info, TokenAccount>,
     #[account(
         mut,
         close = maker,
@@ -34,20 +30,17 @@ pub struct Refund<'info> {
         seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
         bump = escrow.bump
     )]
-    pub escrow: Account<'info, Escrow>,
-
+    escrow: Account<'info, Escrow>,
     #[account(
         mut,
         associated_token::mint = mint_a,
         associated_token::authority = escrow,
-        associated_token::token_program = token_program,
+        associated_token::token_program = token_program
     )]
     pub vault: InterfaceAccount<'info, TokenAccount>,
-
-    pub associated_token_program: Program<'info, AssociatedToken>,
-    pub token_program: Interface<'info, TokenInterface>,
-    pub system_program: Program<'info, System>,
-
+    associated_token_program: Program<'info, AssociatedToken>,
+    token_program: Interface<'info, TokenInterface>,
+    system_program: Program<'info, System>,
 }
 
 impl<'info> Refund<'info> {
@@ -59,16 +52,20 @@ impl<'info> Refund<'info> {
             &[self.escrow.bump],
         ]];
 
-        let transfer_accounts = TransferChecked {
+        let xfer_accounts = TransferChecked {
             from: self.vault.to_account_info(),
             mint: self.mint_a.to_account_info(),
             to: self.maker_ata_a.to_account_info(),
-            authority: self.escrow.to_account_info()
+            authority: self.escrow.to_account_info(),
         };
 
-        let transfer_cpi_ctx = CpiContext::new_with_signer(self.token_program.to_account_info(), transfer_accounts, &signer_seeds);
+        let ctx = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            xfer_accounts,
+            &signer_seeds,
+        );
 
-        transfer_checked(transfer_cpi_ctx, self.vault.amount, self.mint_a.decimals)?;
+        transfer_checked(ctx, self.vault.amount, self.mint_a.decimals)?;
 
         let close_accounts = CloseAccount {
             account: self.vault.to_account_info(),
@@ -76,9 +73,12 @@ impl<'info> Refund<'info> {
             authority: self.escrow.to_account_info(),
         };
 
-        let close_cpi_ctx = CpiContext::new_with_signer(self.token_program.to_account_info(), close_accounts, &signer_seeds);
+        let ctx = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            close_accounts,
+            &signer_seeds,
+        );
 
-        close_account(close_cpi_ctx)
-
+        close_account(ctx)
     }
 }
